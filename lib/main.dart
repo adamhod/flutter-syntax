d63ce6c16ed038cee127676884d80470b00f5b1f
Adam Hoddinott <hodd0022@algonquinlive.com>
2024-07-11 16:31:06 -0400

    example of padding used in container. edgeInsets.all/fromLTRB/symmetric

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,24 @@ class Home extends StatelessWidget {
     centerTitle: true,
     backgroundColor: Colors.amber[600],
     ),
-    body: Container(
-      color: Colors.grey[400],
-      //child: Text('hello'),
+    body: Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(20), //apply 20 pixels of padding around text
+          color: Colors.grey[400],
+          child: Text('hello'),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10), //sets padding to designated
+          color: Colors.brown[400],
+          child: Text('hello'),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(10.0,20.0,30.0,40.0), //designates distance of padding on each side from the center
+          color: Colors.lightBlue[400],
+          child: Text('hello'),
+        ),
+      ],
     ),
     floatingActionButton: FloatingActionButton(
     onPressed: () {},
