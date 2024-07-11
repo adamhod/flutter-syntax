5d28bc9fe9ed2500a00439c2b94cb0726197b3ce
Adam Hoddinott <hodd0022@algonquinlive.com>
2024-07-11 16:37:19 -0400

    initial commit for rows

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,9 @@ class Home extends StatelessWidget {
     centerTitle: true,
     backgroundColor: Colors.amber[600],
     ),
-    body: Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(20), //apply 20 pixels of padding around text
-          margin: EdgeInsets.all(30.0), //padding around container
-          color: Colors.grey[400],
-          child: Text('hello'),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10), //sets padding to designated
-          margin: EdgeInsets.all(5.0),
-          color: Colors.brown[400],
-          child: Text('hello'),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10.0,20.0,30.0,40.0), //designates distance of padding on each side from the center
-          color: Colors.lightBlue[400],
-          child: Text('hello'),
-        ),
-      ],
+    body: Padding(
+      padding: EdgeInsets.all(90.0),
+      child: Text('hello'),
     ),
     floatingActionButton: FloatingActionButton(
     onPressed: () {},
